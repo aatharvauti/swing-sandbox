b34ebc55bc42ec39106b84f3ed45f39b5a4b00b3
aatharvauti <bm.aatharvauti@protonmail.ch>
2021-11-28 14:59:11 +0530

    Chapter 1: Frames

---
+++ apps/src/apps/Main.java
@@ -0,0 +1,29 @@
+package apps;
+
+//import javax.swing.*;
+//import java.awt.Color;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+//		JFrame frame = new JFrame(); // create instance of JFrame
+//
+//		frame.setTitle("JFrame title goes here :D"); // set title
+//		
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of app
+//		// .DO_NOTHING_ON_CLOSE will prevent the user from closing application (lmao)
+//		
+//		frame.setResizable(false); // prevent frame from being resized
+//        frame.setSize(420,420); // sets x and y dimension of frame
+//		
+//        frame.setVisible(true); // make frame visible
+//		
+//		ImageIcon image = new ImageIcon("logo.png"); // create ImageIcon
+//		frame.setIconImage(image.getImage()); // change icon of frame (or favicon if u web dev)
+//		
+//		frame.getContentPane().setBackground(new Color(125,50,255)); // change background color
+		
+		new MyFrame();
+	}
+}
---
+++ apps/src/apps/MyFrame.java
@@ -0,0 +1,30 @@
+package apps;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	
+	MyFrame() {
+
+
+		this.setTitle("JFrame title goes here :D"); // set title
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of app
+		// .DO_NOTHING_ON_CLOSE will prevent the user from closing application (lmao)
+		
+		this.setResizable(false); // prevent this from being resized
+        this.setSize(420,420); // sets x and y dimension of this
+		
+        this.setVisible(true); // make this visible
+		
+		ImageIcon image = new ImageIcon("logo.png"); // create ImageIcon
+		this.setIconImage(image.getImage()); // change icon of this (or favicon if u web dev)
+		
+		this.getContentPane().setBackground(new Color(125,50,255)); // change background color
+
+	}
+
+}
