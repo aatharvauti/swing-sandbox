86441c7053c0c9e6675dde16de131ef75937be7a
aatharvauti <bm.aatharvauti@protonmail.ch>
2021-11-28 19:09:03 +0530

    Chapter 3: Panels

--- apps/src/apps/Main.java
+++ apps/src/apps/Main.java
@@ -11,6 +11,8 @@ public class Main
 	{
 		// Chapter 1: Frames
 		
+//		new MyFrame();
+		
 		JFrame frame = new JFrame(); // create instance of JFrame
 
 		frame.setTitle("JFrame title goes here :D"); // set title
@@ -19,9 +21,9 @@ public class Main
 		// .DO_NOTHING_ON_CLOSE will prevent the user from closing application (lmao)
 		
 		frame.setResizable(false); // prevent frame from being resized
-//        frame.setSize(420,420); // sets x and y dimension of frame
+//      frame.setSize(420,420); // sets x and y dimension of frame
 		
-        frame.setVisible(true); // make frame visible
+		frame.setVisible(true); // make frame visible
 		
 		ImageIcon image = new ImageIcon("/home/syn4ck/code/swing/apps/src/apps/logo.png"); // create ImageIcon
 //		ImageIcon image = new ImageIcon("/apps/src/apps/logo.png");
@@ -65,6 +67,22 @@ public class Main
 		
 		frame.add(label); // add label to the frame
 		frame.pack(); // resize size of frame to fit contents
-//		new MyFrame();
+		
+		// Chapter 3: Panels
+		
+//		JPanel redpanel = new JPanel(); // create a panel
+//		
+//		redpanel.setBackground(Color.red); // add bg color
+//		redpanel.setBounds(0,0,250,250); // set bounds (x,y,x-d,y-d)
+//		redpanel.add(label); // add "label" to the "panel" 
+//		redpanel.setLayout(new BorderLayout()); // use layout manager, if not using bounds
+//
+//		frame.add(redpanel); //add "redpanel" to the "frame"
+	
+		// Chapter 4: Buttons
+		
+		
+		
+		
 	}
 }
